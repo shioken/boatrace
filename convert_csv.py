9a1acdc6da194940e53b374b9caadaa5fbf00e59
shioken <kasehonitan@gmail.com>
2021-05-29 11:10:22 +0900

    CSVのフォーマット変更

--- convert_csv.py
+++ convert_csv.py
@@ -11,13 +11,28 @@ def convertFile(file):
     with open(file, 'rt', encoding='utf-8') as f:
         data = json.load(f)
 
+        scores = (8, 3, 1)
+
         rows = []
         for race in data:
-            row = []
-            row += [race["place"], race["placeid"], race["racenumber"]]
+            result = race["result"]
+            results = result["results"]
+            print(results)
+            # row.extend(results)
+
             racers = race["racers"]
-            for racer in racers:
-                row.extend([
+            for i, racer in enumerate(racers):
+                row = []
+
+                score = 0
+                if (i + 1) in results:
+                    r = results.index(i + 1)
+                    score = scores[r]
+
+                row += [score]
+
+                row += [race["place"], race["placeid"], race["racenumber"]]
+                row += [
                     racer["number"],
                     racer["name"],
                     racer["age"],
@@ -28,21 +43,23 @@ def convertFile(file):
                     racer["sec_all"],
                     racer["win_cur"],
                     racer["sec_cur"],
-                ])
+                ]
 
-            result = race["result"]
-            results = result["results"]
-            row.extend(results)
-
-            rows.append(row)
+                if results[0] > -1:
+                    rows.append(row)
+                else:
+                    print("skip race(中止)")
 
         with open(csvfilename, 'wt', encoding='utf-8') as w:
             writer = csv.writer(w)
 
-            header = ["place", "placeid", "racenumber", ]
-            for i in range(1, 7):
-                header += [f"number{i}", f"name{i}", f"age{i}", f"area{i}", f"weight{i}", f"rank{i}", f"win_all{i}", f"sec_all{i}", f"win_cur{i}", f"sec_cur{i}"]
-            header += ["1st", "2nd", "3rd"]
+            header = []
+            # header += ["1st", "2nd", "3rd"]
+            header += ["winscore"]
+            header += ["place", "placeid", "racenumber", ]
+            header += ["number", "name", "age", "area", "weight", "rank", "win_all", "sec_all", "win_cur", "sec_cur"]
+            # for i in range(1, 7):
+            #     header += [f"number{i}", f"name{i}", f"age{i}", f"area{i}", f"weight{i}", f"rank{i}", f"win_all{i}", f"sec_all{i}", f"win_cur{i}", f"sec_cur{i}"]
 
             writer.writerow(header)            
             writer.writerows(rows)
@@ -54,6 +71,6 @@ def convertFiles(files):
         convertFile(file)
 
 if __name__ == '__main__':
-    files = glob.glob('model/*.json')
-    # files = ['model/m210525.json']
+    # files = glob.glob('model/*.json')
+    files = ['model/m210525.json']
     convertFiles(files)
