91e90c1bfc9d843223b6a52552b9e0af0587f691
shioken <kasehonitan@gmail.com>
2021-06-02 23:14:32 +0900

    日付を出力

--- inquire.py
+++ inquire.py
@@ -13,6 +13,7 @@ def search_result(result, place, number):
     return None
 
 def inquire(target):
+
     votefile = f'votes/v{target}.json'
     resultfile = f'json/m{target}.json'
     
@@ -23,6 +24,10 @@ def inquire(target):
     if not os.path.exists(resultfile):
         print(f'{resultfile} is not exists')
         return
+
+    year = 2000 + int(target[:2])
+    month = int(target[2:4])
+    day = int(target[4:])
 
     with open(votefile, 'r', encoding='utf-8') as vf, open(resultfile, 'r', encoding='utf-8') as rf:
         vjson = json.load(vf)
@@ -76,8 +81,9 @@ def inquire(target):
                     print(f"{race['number']}R レース不成立")
 
 
-        print("")        
-        print(f"レース数:{total_race}, 総投資額:{total_bet:8,} 3連単({tierce_count:2}):{tierce_inquire:8,}({tierce_inquire - total_bet:8,}) 3連複({trio_count:2}):{trio_inquire:8,}(({trio_inquire - total_bet:8,})")
+        print("")
+        print(f"{year}/{month:02}/{day:02}")
+        print(f"レース数:{total_race}, 総投資額:{total_bet:8,} 3連単({tierce_count:2}):{tierce_inquire:8,}({tierce_inquire - total_bet:8,}) 3連複({trio_count:2}):{trio_inquire:8,}({trio_inquire - total_bet:8,})")
         print(f"当選率 3連単:{tierce_count / total_race * 100:6.2f}% 3連複:{trio_count / total_race * 100:6.2f}%")
         print(f"回収率 3連単:{tierce_inquire / total_bet * 100:6.2f}% 3連複:{trio_inquire / total_bet * 100:6.2f}%")
 
