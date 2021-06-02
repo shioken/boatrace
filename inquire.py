97f4d5da8a34f3344f36514a149b6ebc95a73b22
shioken <kasehonitan@gmail.com>
2021-06-03 00:12:38 +0900

    投票閾値を指定できるように修正

--- inquire.py
+++ inquire.py
@@ -12,7 +12,7 @@ def search_result(result, place, number):
 
     return None
 
-def inquire(target):
+def inquire(target, minthreshold = 0.0, maxthreshold = 1.0):
 
     votefile = f'votes/v{target}.json'
     resultfile = f'json/m{target}.json'
@@ -35,8 +35,14 @@ def inquire(target):
 
         tierce_count = 0
         tierce_inquire = 0
+        tierce_std = 0.0
         trio_count = 0
         trio_inquire = 0
+        trio_std = 0.0
+        out_tierce_std = 0.0
+        out_tierce_count = 0
+        out_trio_std = 0.0
+        out_trio_count = 0
         total_bet = 0
         total_race = 0
 
@@ -53,44 +59,72 @@ def inquire(target):
                     # print(f"{race['number']}R 3連単: {order[0]}-{order[1]}-{order[2]} 配当{result['tierce']:6,} / 3連複: {sorted_order[0]}-{sorted_order[1]}-{sorted_order[2]} 配当 {result['trio']:6,}")
                     vline = f"{race['number']:2}R 3連単: {order[0]}-{order[1]}-{order[2]} 配当{result['tierce']:6,} / 3連複: {sorted_order[0]}-{sorted_order[1]}-{sorted_order[2]} 配当 {result['trio']:6,}"
                     vline += " "
-                    for vote in race["votes"]:
-                        bet = (int(vote[0:1]), int(vote[2:3]), int(vote[4:5]))
-                        trio_bet = sorted(bet)
+                    if race["std"] >= minthreshold and race["std"] <= maxthreshold:
+                        isRaceWin = [False, False]
+                        for vote in race["votes"]:
+                            bet = (int(vote[0:1]), int(vote[2:3]), int(vote[4:5]))
+                            trio_bet = sorted(bet)
 
-                        total_bet += 100
+                            total_bet += 100
 
-                        vline += vote
-                        if bet == order:
-                            vline += '*'
-                            tierce_count += 1
-                            tierce_inquire += result['tierce']
-                            trio_inquire += result['trio']
-                            trio_count += 1
-                        elif sorted_order == trio_bet:
-                            vline += '#'
-                            trio_count += 1
-                            trio_inquire += result['trio']
-                        else:
+                            vline += vote
+                            if bet == order:
+                                vline += '*'
+
+                                tierce_count += 1
+                                tierce_inquire += result['tierce']
+                                tierce_std += race["std"]
+
+                                trio_count += 1
+                                trio_inquire += result['trio']
+                                trio_std += race["std"]
+
+                                isRaceWin[0] = True
+                                isRaceWin[1] = True
+                            elif sorted_order == trio_bet:
+                                vline += '#'
+
+                                trio_count += 1
+                                trio_inquire += result['trio']
+                                trio_std += race["std"]
+
+                                isRaceWin[1] = True
+                            else:
+                                vline += " "
+                            
                             vline += " "
-                        
-                        vline += " "
-                    print(vline)
 
-                    total_all_bet += result['tierce']
+                        if isRaceWin[0] is False:
+                            out_tierce_std += race["std"]
+                            out_tierce_count += 1
+                        if isRaceWin[1] is False:
+                            out_trio_std += race["std"]
+                            out_trio_count += 1
+
+                        print(vline)
+
+                        total_all_bet += result['tierce']
                 else:
                     print(f"{race['number']}R レース不成立")
 
 
         print("")
         print(f"{year}/{month:02}/{day:02}")
-        print(f"レース数:{total_race}, 総投資額:{total_bet:8,} 3連単({tierce_count:2}):{tierce_inquire:8,}({tierce_inquire - total_bet:8,}) 3連複({trio_count:2}):{trio_inquire:8,}({trio_inquire - total_bet:8,})")
-        print(f"当選率 3連単:{tierce_count / total_race * 100:6.2f}% 3連複:{trio_count / total_race * 100:6.2f}%")
-        print(f"回収率 3連単:{tierce_inquire / total_bet * 100:6.2f}% 3連複:{trio_inquire / total_bet * 100:6.2f}%")
+        if total_bet > 0 and total_race > 0:
+            print(f"レース数:{total_race}, 総投資額:{total_bet:8,} 3連単({tierce_count:2}):{tierce_inquire:8,}({tierce_inquire - total_bet:8,}) 3連複({trio_count:2}):{trio_inquire:8,}({trio_inquire - total_bet:8,})")
+            print(f"当選率 3連単:{tierce_count / total_race * 100:6.2f}% 3連複:{trio_count / total_race * 100:6.2f}%")
+            print(f"回収率 3連単:{tierce_inquire / total_bet * 100:6.2f}% 3連複:{trio_inquire / total_bet * 100:6.2f}%")
 
         print("")
-        print(f"3連単全掛: {total_all_bet:8,} 回収率: {total_all_bet / (total_race * 100 * 120) * 100:6.2f}%")
+        if tierce_count > 0 and trio_count > 0:
+            print(f"3連単 当選標準偏差:{tierce_std / tierce_count:1.5f}/非当選:{out_tierce_std / out_tierce_count:1.5f} 3連複 当選標準偏差:{trio_std / trio_count:1.5f}/非当選:{out_trio_std / out_trio_count:1.5f}")
+
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 4:
+        inquire(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]))
+    if len(sys.argv) == 3:
+        inquire(sys.argv[1], float(sys.argv[2]))
+    elif len(sys.argv) == 2:
         inquire(sys.argv[1])
     else:
         print("Please specify a file.")
