1b34ed24b1bd6dc932d3a4bd5972738e66fb6f3c
shioken <kasehonitan@gmail.com>
2021-06-03 17:04:36 +0900

    日付指定したすべてのレースのオッズをもとにした予測を実行する

---
+++ check_all_ods.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import glob
+import os
+import sys
+import check_odds as co
+
+def check_all_odds(date):
+    files = glob.glob(f'odds/o{date}*.json')
+    print(len(files))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        check_all_odds(sys.argv[1])
+    else:
+        print("check_all_odds date")
